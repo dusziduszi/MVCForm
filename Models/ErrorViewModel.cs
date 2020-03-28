54be58755bc4d9a95ddc50d5104b8d9fa3c84e5c
Martyna Duszyńska <martyna.duszynska29@gmail.com>
2020-03-28 17:15:58 +0100

    first repo

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVCForm.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
